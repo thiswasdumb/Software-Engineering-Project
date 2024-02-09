347f558a5ef6a92a2539ef8ed790261e68b4158c
hapaxlegomena <thesemariofilms@gmail.com>
2024-02-09 14:00:56 +0000

    Created basic structure of pages (WIP)

--- tradetalker/app/layout.tsx
+++ tradetalker/app/layout.tsx
@@ -1,5 +1,6 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import Navigation from './components/navigation/navbar';
+import './globals.css';
+import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,5 +18,5 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
