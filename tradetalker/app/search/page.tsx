377861679bc911de821585ae60a2fe1e3d375de4
hapaxlegomena <thesemariofilms@gmail.com>
2024-02-16 15:30:49 +0000

    New changes

--- tradetalker/app/search/page.tsx
+++ tradetalker/app/search/page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Metadata } from "next";
+import SearchBar from "@/app/ui/search";
 
 export const metadata: Metadata = {
   title: "Search",
 };
 
 export default function Search() {
-  return <p>Search</p>;
+  return (
+    <Search />
+  );
 }
