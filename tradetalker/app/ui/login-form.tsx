d6755fa448d9e41b486396ba641720b2b43cdabd
hapaxlegomena <thesemariofilms@gmail.com>
2024-02-27 08:23:59 +0000

    Updated login form + client-side redirect

--- tradetalker/app/ui/login-form.tsx
+++ tradetalker/app/ui/login-form.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import React, { useState, useEffect, FormEvent, ChangeEvent } from 'react';
 import {
   AtSymbolIcon,
   KeyIcon,
@@ -7,74 +7,142 @@ import {
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
-import { useFormState, useFormStatus } from 'react-dom';
-import { authenticate } from '@/app/lib/actions';
+import { useFormStatus } from 'react-dom';
+import { useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+import { toast } from 'react-hot-toast';
 
-export default function LoginForm() {
-  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
+export default function LoginForm({ isLoggedIn }: { isLoggedIn: boolean }) {
+  const [errorMessage, setErrorMessage] = useState('');
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const [data, setData] = useState({});
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // Prevent the default form submission
+    try {
+      const response = await fetch('api/login_form', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+      const error = await response.json(); // Parse the JSON response
+      if (error.error) {
+        setErrorMessage(error.error);
+        setTimeout(() => {
+          setErrorMessage('');
+        }, 6000);
+      }
+      if (error.success) {
+        router.push('/dashboard');
+        toast.success(error.success);
+        router.refresh();
+      }
+    } catch (error) {
+      toast.error('An error occurred. Please try again later.');
+    }
+  };
+  // Set error message if there is an error in the URL parameters
+  useEffect(() => {
+    if (!isLoggedIn) {
+      const error = searchParams.get('error');
+      if (error) {
+        toast.error(error);
+        router.replace('/login', undefined);
+      }
+    } else {
+      router.push('/dashboard');
+      toast.error('You are already logged in.');
+    }
+  }, [router, searchParams, isLoggedIn]);
+
   return (
-    <form action={dispatch} className='space-y-3'>
-      <div className='flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8'>
-        <h1 className='mb-3 text-2xl'>
-          Log in to continue your TradeTalker journey.
-        </h1>
-        <div className='w-full'>
-          <div>
-            <label
-              className='mb-3 mt-5 block text-base font-medium text-gray-900'
-              htmlFor='username'
+    <>
+      {!isLoggedIn && (
+        <div className='mt-10 flex items-center justify-center'>
+          <div className='mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4'>
+            <form
+              onSubmit={handleSubmit}
+              method='post'
+              className='space-y-3'
+              autoComplete='off'
             >
-              Username
-            </label>
-            <div className='relative'>
-              <input
-                className='relative block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500'
-                id='username'
-                type='text'
-                name='username'
-                placeholder='Enter your username'
-                required
-              />
-              <AtSymbolIcon className='pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900' />
-            </div>
-          </div>
-          <div className='mt-4'>
-            <label
-              className='mb-3 mt-5 block text-base font-medium text-gray-900'
-              htmlFor='password'
-            >
-              Password
-            </label>
-            <div className='relative'>
-              <input
-                className='relative block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500'
-                id='password'
-                type='password'
-                name='password'
-                placeholder='Enter password'
-                required
-                minLength={6}
-              />
-              <KeyIcon className='pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900' />
-            </div>
+              <div className='flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8'>
+                <h1 className='mb-3 text-2xl'>
+                  Log in to continue your TradeTalker journey.
+                </h1>
+                <div className='w-full'>
+                  <div>
+                    <label
+                      className='mb-3 mt-5 block text-base font-medium text-gray-900'
+                      htmlFor='username'
+                    >
+                      Username
+                    </label>
+                    <div className='relative'>
+                      <input
+                        className='relative block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500'
+                        id='username'
+                        type='text'
+                        name='username'
+                        placeholder='Enter your username'
+                        onChange={handleChange}
+                        minLength={3}
+                        maxLength={50}
+                        required
+                      />
+                      <AtSymbolIcon className='pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900' />
+                    </div>
+                  </div>
+                  <div className='mt-4'>
+                    <label
+                      className='mb-3 mt-5 block text-base font-medium text-gray-900'
+                      htmlFor='password'
+                    >
+                      Password
+                    </label>
+                    <div className='relative'>
+                      <input
+                        className='relative block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500'
+                        id='password'
+                        type='password'
+                        name='password'
+                        placeholder='Enter password'
+                        onChange={handleChange}
+                        required
+                        minLength={8}
+                        maxLength={200}
+                        pattern='(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}'
+                      />
+                      <KeyIcon className='pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900' />
+                    </div>
+                  </div>
+                </div>
+                <LoginButton />
+                <ForgotPassword />
+                <div
+                  className='flex h-8 items-end space-x-1 overflow-hidden'
+                  aria-live='polite'
+                  aria-atomic='true'
+                >
+                  {errorMessage && (
+                    <>
+                      <ExclamationCircleIcon className='h-5 w-5 text-red-500' />
+                      <p className='text-sm text-red-500'>{errorMessage}</p>
+                    </>
+                  )}
+                </div>
+              </div>
+            </form>
           </div>
         </div>
-        <LoginButton />
-        <ForgotPassword />
-        <div
-          className='flex h-8 items-end space-x-1'
-          aria-live='polite'
-          aria-atomic='true'
-        >
-          {errorMessage && (
-            <>
-              <ExclamationCircleIcon className='h-5 w-5 text-red-500' />
-              <p className='text-sm text-red-500'>{errorMessage}</p>
-            </>
-          )}
-        </div>
-      </div>
-    </form>
+      )}
+    </>
   );
 }
 
