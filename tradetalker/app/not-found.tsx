252148690f88b4f0630efa6eb1034b4f8396169e
hapaxlegomena <thesemariofilms@gmail.com>
2024-02-27 08:10:53 +0000

    Updated 404 page

--- tradetalker/app/not-found.tsx
+++ tradetalker/app/not-found.tsx
@@ -1,16 +1,23 @@
-import Link from 'next/link';
+'use client';
 import { Metadata } from 'next';
+import { useRouter } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: 'Not Found',
 };
 
 export default function NotFound() {
+  const router = useRouter();
   return (
-    <div className=''>
-      <Link href='/page-not-found'>Not Found</Link>
+    <div className='flex flex-col justify-center rounded-lg bg-slate-200 p-4'>
       <p>Could not find requested resource.</p>
-      <Link href='/'>Return Home</Link>
+      <button
+        type='button'
+        onClick={() => router.back()}
+        className='rounded-lg bg-blue-500 p-4 text-white transition hover:bg-blue-600'
+      >
+        Back
+      </button>
     </div>
   );
 }
