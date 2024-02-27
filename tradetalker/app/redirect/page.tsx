c77014061c3c02728bac187f43d1502faec59099
hapaxlegomena <thesemariofilms@gmail.com>
2024-02-27 08:21:40 +0000

    Update redirect

--- tradetalker/app/redirect/page.tsx
+++ tradetalker/app/redirect/page.tsx
@@ -6,8 +6,18 @@ export default function RedirectPage() {
   const router = useRouter();
   useEffect(() => {
     // Redirect to another page
-    router.push('http://localhost:8080/api/redirect');
+    router.push('/api/redirect');
   }, [router]);
 
-  return <div>Redirecting...</div>;
+  return (
+    <div className='m-8 rounded-lg bg-slate-200 p-8'>
+      <div className='text-2xl'>Redirecting...</div>
+      <hr className='my-2 rounded-lg border-2 border-slate-400' />
+    </div>
+  );
+}
+
+export function RedirectWithMessage(destination: string, message: string) {
+  localStorage.setItem('message', message); // Store message for one session
+  window.location.href = destination; // Redirect to another page
 }
