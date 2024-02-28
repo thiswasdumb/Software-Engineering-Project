1c4cb8c0252e71685f5ede64e31eb01bf80e8d9d
XKMonash <126446473+XKMonash@users.noreply.github.com>
2024-02-28 16:35:50 +0000

    Changed TF_IDF attribute

--- tradetalker/python_component/Database/tf_idf.py
+++ tradetalker/python_component/Database/tf_idf.py
@@ -22,14 +22,18 @@ class TF_IDF():
 
     def _generate_tf_idf_matrix(self):
         # get article index array 
-        article_index = [f"Article {i+1}" for i in range(len(articles))]
+        article_index = [f"Article {i+1}" for i in range(self.num_articles)]
         self.df_tf_idf = pd.DataFrame(self.tfidf_scores.T.todense(), index=self.feature_names, columns=article_index)
-        self.get_top_n_terms(10)
+
 
     def get_top_n_terms(self, n:int):
         for i in range(1, self.num_articles+1):  
-            top_terms = self.df_tf_idf[[f'Article {i}']].nlargest(n, columns=[f'Article {i}'])
-            print(top_terms)
+            top_terms = self.df_tf_idf[[f'Article {i}']].nlargest(n, columns=[f'Article {i}'])            
+            # print(top_terms)
+            top_words =  top_terms[f'Article {i}']
+            print(top_words)
+            print(top_words.to_list())
+
 
 get_pos_class = GetPOSClass() 
 preprocess_text = PreprocessText(get_pos_class)
@@ -37,7 +41,7 @@ preprocess_text = PreprocessText(get_pos_class)
 processed_articles = [preprocess_text.preprocess_text(a) for a in articles.keys()]
 
 tf_idf_object = TF_IDF(processed_articles)
-tf_idf_object.get_top_n_terms(5)
+tf_idf_object.get_top_n_terms(10)
 
 
 
