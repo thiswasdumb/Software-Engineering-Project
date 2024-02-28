247efb71ad74d77d516083ee2d52301dea6b5c20
gnh1912 <159541945+gnh1912@users.noreply.github.com>
2024-02-28 14:19:07 +0000

    Update Linear_Regression.py

--- tradetalker/python_component/Linear_Regression/Linear_Regression.py
+++ tradetalker/python_component/Linear_Regression/Linear_Regression.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error, r2_score
+from sklearn.metrics import mean_absolute_error
 import matplotlib.pyplot as plt
 
 # from database_connection import DataBaseConnection
