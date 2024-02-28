dd9dd3e872a15eec8af76b5e3de64bbbf1ce0b10
gnh1912 <159541945+gnh1912@users.noreply.github.com>
2024-02-28 14:02:45 +0000

    Update Linear_Regression.py

--- tradetalker/python_component/Linear_Regression/Linear_Regression.py
+++ tradetalker/python_component/Linear_Regression/Linear_Regression.py
@@ -6,21 +6,47 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, r2_score
 import matplotlib.pyplot as plt
-# from TradeTalker_DB import Database
 
-# Initialising Flask application 
-app = Flask(__name__)
+# from database_connection import DataBaseConnection
+# db = DataBaseConnection (
+#     host="localhost",
+#         user="root",
+#         passwd="",
+#         database="tradetalkerdb"
+#     )
 
-# Configuration for MySQL database connection
-# 'TradeTalkerDB' is the name of the MySQL database
-# 'DictCursor' returns rows as dictionaries, making them easier to work with
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'TradeTalkerDB'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# Work out CAPM equation
+# Importing numpy library and aliasing it as np for ease of use
+import numpy as np
 
-# db = Database(app)
+# Defining market_data and stock_data arrays using numpy arrays
+market_data = np.array([-0.685,-0.438,-0.302,0.015,-0.623])
+stock_data = np.array([-0.419,-6.791,-0.635,-2.737,1.278])
+
+# Defining bond_yield, inflation, and calculating riskfree rate
+bond_yield = 4.65
+inflation = 4.2
+riskfree = bond_yield - inflation
+
+# Calculating covariance, variance, and mean of market_data
+# Covariance between market_data and stock_data
+cv = np.cov(market_data, stock_data)[0][1].item()
+
+# Variance of market_data
+mv = np.var(market_data).item()
+
+# Mean of market_data
+mr = np.mean(market_data).item()
+
+# Calculating beta using CAPM formula
+# Beta coefficient
+b = (mr * cv) / mv
+
+ # Capital Asset Pricing Model (CAPM) calculation
+capm = riskfree + b * (mr - riskfree)
+
+# Printing the calculated CAPM value
+print(capm)
 
 # Sample data for testing
 # Generate random data with a weak trend
