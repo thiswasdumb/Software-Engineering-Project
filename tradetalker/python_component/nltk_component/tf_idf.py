4df46785ff1c2ea93232f25dda13634f67ec593d
XKMonash <126446473+XKMonash@users.noreply.github.com>
2024-03-02 17:50:36 +0000

    Added tests for search, Vader + preprocessing, TF_DIF
    changed data format and added some methods

--- tradetalker/python_component/nltk_component/tf_idf.py
+++ tradetalker/python_component/nltk_component/tf_idf.py
@@ -25,29 +25,51 @@ class TF_IDF():
         article_index = [f"Article {i+1}" for i in range(self.num_articles)]
         self.df_tf_idf = pd.DataFrame(self.tfidf_scores.T.todense(), index=self.feature_names, columns=article_index)
 
+    def get_top_n_terms(self, n:int, article_id: int):
+        #sorting the columns (each article) by their values (tf-idf score) for each term/word/feature
+        top_terms = self.df_tf_idf[[f'Article {article_id}']].nlargest(n, columns=[f'Article {article_id}'])            
+        #retrieving only the feature names (words)
+        top_words = top_terms[[f'Article {article_id}']].index
 
-    def get_top_n_terms(self, n:int):
+        # print(f"Top {n} terms for Article {i}:")
+        # for index, score in zip(top_terms.index, top_terms[f'Article {i}']):
+        #     print(f"{index}: {score}")
+
+        top_words_str = ",".join([w for w in top_words])
+        # print(top_words_str)
+        return top_words_str
+    
+    
+    def get_top_n_terms_tfidf_scores(self, n:int, article_id: int):
+        #sorting the columns (each article) by their values (tf-idf score) for each term/word/feature
+        top_terms = self.df_tf_idf[[f'Article {article_id}']].nlargest(n, columns=[f'Article {article_id}'])            
+        #retrieving only the feature columns (tf_idf score)
+        return top_terms[[f'Article {article_id}']].values.tolist()
+
+
+    def get_top_n_terms_for_all_articles(self, n:int):
         for i in range(1, self.num_articles+1): 
             #sorting the columns (each article) by their values (tf-idf score) for each term/word/feature
             top_terms = self.df_tf_idf[[f'Article {i}']].nlargest(n, columns=[f'Article {i}'])            
             #retrieving only the feature names (words)
             top_words = top_terms[[f'Article {i}']].index
 
-            print(f"Top {n} terms for Article {i}:")
-            for index, score in zip(top_terms.index, top_terms[f'Article {i}']):
-                print(f"{index}: {score}")
+            # print(f"Top {n} terms for Article {i}:")
+            # for index, score in zip(top_terms.index, top_terms[f'Article {i}']):
+            #     print(f"{index}: {score}")
 
             top_words_str = ",".join([w for w in top_words])
-            print(top_words_str)
+            # print(top_words_str)
+            return top_words_str
 
 
-get_pos_class = GetPOSClass() 
-preprocess_text = PreprocessText(get_pos_class)
-# preprocessed articles, this will be the input for this module 
-processed_articles = [preprocess_text.preprocess_text(a) for a in articles.keys()]
+# get_pos_class = GetPOSClass() 
+# preprocess_text = PreprocessText(get_pos_class)
+# # preprocessed articles, this will be the input for this module 
+# processed_articles = [preprocess_text.preprocess_text(a) for a in articles.keys()]
 
-tf_idf_object = TF_IDF(processed_articles)
-tf_idf_object.get_top_n_terms(10)
+# tf_idf_object = TF_IDF(processed_articles)
+# tf_idf_object.get_top_n_terms(10)
 
 
 
---
+++ tradetalker/tests/tf_idf_unit_tests.py
@@ -0,0 +1,39 @@
+import unittest
+from unittest.mock import patch
+
+import sys
+sys.path.insert(0, '/Users/mac/Documents/GitHub/SoftEngProject/tradetalker')
+
+from python_component.nltk_component.search_component import ArticleSearch
+from python_component.nltk_component.tf_idf import TF_IDF
+from python_component.nltk_component.preprocessing import GetPOSClass, PreprocessText
+from vader import SentimentAnalyser
+from testing_articles import articles, articles_header, articles_list  # Testing dictionary key=article, value= positive/neutral/negative (human classified)
+    
+
+
+class TestTF_IDF(unittest.TestCase):
+    #tests that the top n words returned by the function is accurate by 
+    #by calculating their tf_idf scores and verifying that the words are ordered by highest tf_idf score (most important) to lowest 
+    def setUp(self):
+        self.sample_tests = articles  # list of 10 articles (preprocessed )
+        self.tf_idf = TF_IDF(articles_list)
+        get_pos_class = GetPOSClass() 
+        self.preprocess_text = PreprocessText(get_pos_class)
+        self.s = SentimentAnalyser()
+
+    def test_keywords(self):
+        article_index = 1 
+        for article, expected_sentiment in self.sample_tests.items():
+            # Mocking the external dependency for SentimentIntensityAnalyzer.polarity_scores
+            with self.subTest():
+                top_20_search_terms = self.tf_idf.get_top_n_terms(20, article_index).split(',')
+                top_20_search_terms_scores = self.tf_idf.get_top_n_terms_tfidf_scores(20, article_index)
+                print("Search terms:", top_20_search_terms)
+                print("Search term scores:", top_20_search_terms_scores)
+                self.assertTrue(all(top_20_search_terms_scores[i] >= top_20_search_terms_scores[i + 1] for i in range(len(top_20_search_terms_scores)-1)))
+            article_index += 1
+
+if __name__ == '__main__':
+    unittest.main()
+
