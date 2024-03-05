774fbd2a24aab879b7cd1a0a672459ef58baad53
XKMonash <126446473+XKMonash@users.noreply.github.com>
2024-03-05 18:30:35 +0000

    Changed data format, fixed tf_idf matrix mapping for articles
    Made new main functions, added comments, extract_news_script returns list of dicts for all articles. Added TextSummariser to summarise text since newspaper3k .summary does not work.

--- tradetalker/tests/tf_idf_unit_tests.py
+++ tradetalker/tests/tf_idf_unit_tests.py
@@ -4,7 +4,7 @@ from unittest.mock import patch
 import sys
 sys.path.insert(0, '/Users/mac/Documents/GitHub/SoftEngProject/tradetalker')
 
-from python_component.nltk_component.search_component import ArticleSearch
+
 from python_component.nltk_component.tf_idf import TF_IDF
 from python_component.nltk_component.preprocessing import GetPOSClass, PreprocessText
 from vader import SentimentAnalyser
@@ -23,16 +23,16 @@ class TestTF_IDF(unittest.TestCase):
         self.s = SentimentAnalyser()
 
     def test_keywords(self):
-        article_index = 1 
+        article_id = 1 
         for article, expected_sentiment in self.sample_tests.items():
             # Mocking the external dependency for SentimentIntensityAnalyzer.polarity_scores
             with self.subTest():
-                top_20_search_terms = self.tf_idf.get_top_n_terms(20, article_index).split(',')
-                top_20_search_terms_scores = self.tf_idf.get_top_n_terms_tfidf_scores(20, article_index)
+                top_20_search_terms = self.tf_idf.get_top_n_terms(20, article_id).split(',')
+                top_20_search_terms_scores = self.tf_idf.get_top_n_terms_tfidf_scores(20, article_id)
                 print("Search terms:", top_20_search_terms)
                 print("Search term scores:", top_20_search_terms_scores)
                 self.assertTrue(all(top_20_search_terms_scores[i] >= top_20_search_terms_scores[i + 1] for i in range(len(top_20_search_terms_scores)-1)))
-            article_index += 1
+            article_id += 1
 
 if __name__ == '__main__':
     unittest.main()
