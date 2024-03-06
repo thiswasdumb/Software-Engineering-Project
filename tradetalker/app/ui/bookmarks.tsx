377e22c32b3de1ab02e6f828c77cea3e58661229
hapaxlegomena <thesemariofilms@gmail.com>
2024-03-06 13:25:15 +0000

    Remove debug log

--- tradetalker/app/ui/bookmarks.tsx
+++ tradetalker/app/ui/bookmarks.tsx
@@ -13,7 +13,6 @@ export default function BookmarkComponent({
   const router = useRouter();
 
   useEffect(() => {
-    console.log(isLoggedIn);
     if (isLoggedIn) {
       const fetchBookmarks = async () => {
         try {
